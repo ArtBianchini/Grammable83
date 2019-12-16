befa4c52f8cca2074346809f945b428a5b65c3e3
Arthur Bianchini <ArtBianchini@gmail.com>
2019-12-16 21:06:52 +0000

    L19 almost there

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.dafault_url_options = { host:  'grammable-arthur-bianchini.herokuapp.com'}
+  config.action_mailer.default_url_options = { host:  'grammable-arthur-bianchini.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
